59d64897a258f302edd123bc446a172a234c24b9
Aaron Blasdel <ablasdel@willowgarage.com>
2012-12-20 11:10:47 -0800

    removed depricated tags from package.xml and updated the setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin_pkg.package import parse_package_for_distutils
+from catkin_pkg.python_setup import generate_distutils_setup
 
-d = parse_package_for_distutils()
-d['packages'] = ['rosdoc_lite']
-d['package_dir'] = {'rosdoc_lite': 'src/rosdoc_lite'}
-d['scripts'] = ['scripts/rosdoc_lite']
-d['package_data'] = {'rosdoc_lite': ['templates/*']}
+d = generate_distutils_setup(
+    packages=['rosdoc_lite'],
+    package_dir={'': 'src'},
+    scripts=['scripts/rosdoc_lite'],
+    package_data={'rosdoc_lite': ['templates/*']}
+)
 
 setup(**d)
